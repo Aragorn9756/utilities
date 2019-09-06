387445065a6f96192928d9eddc6888a8147c3cf6
Aragorn9756 <stephenrichardso@u.boisestate.edu>
2019-09-06 15:14:32 -0600

    structs

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        //structs are objects that act like variables
+        struct Point2D
+        {
+        public int x = 0;
+        public int y = 0;
+        }
+        
+        //how to use a struct like a variable
+        public void AddPoint(Point2D anotherPoint)
+        {
+            this.x = this.x + anotherPoint.x;
+            this.y = this.y + anotherPoint.y;
+        }
+	}
+}
