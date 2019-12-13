bb1fefbfaa0f31bc8cae3a7efff18a63a4857126
Aragorn9756 <45053510+Aragorn9756@users.noreply.github.com>
2019-12-13 03:05:03 -0700

    new code snippets
    Code that uses Coroutines to manipulate timing of code execution and dealing with rigidbodies.

---
+++ Baby.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Baby : MonoBehaviour
+{
+    [HideInInspector]
+    public Transform target;
+    public Transform knifeTrans;
+    public bool knifeGrab = false;
+
+    private NavMeshAgent agent;
+    private bool grabbed = false;
+    private OVRGrabbable grabScript;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        grabScript = this.gameObject.GetComponent<OVRGrabbable>();
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(grabScript.isGrabbed);
+        //move to current target if not grabbed
+        if (grabScript.isGrabbed == false)
+        {
+            agent.destination = target.position;
+            Debug.Log(agent.pathStatus);
+        }
+
+        if (knifeGrab)
+        {
+            knifeTrans.position = this.transform.position;
+        } //else 
+
+        // if grabbed...
+        if (grabScript.isGrabbed)
+        {
+            agent.enabled = false;
+            //make sure that grabbed is set to true
+            if (!grabbed)
+            {
+                grabbed = true;
+                //disable rigidbody temporarily to stop the rigidbody from interacting with the player
+                rb.Sleep();
+
+                //if holding knife, drop it
+                if (GameManager.Instance.hasKnife == true)
+                {
+                    knifeGrab = false;
+
+                    //unfreeze knifes position and reenable the collider
+                    knifeTrans.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                    
+                    
+                    StartCoroutine(KnifeDrop());
+                    GameManager.Instance.hasKnife = false;
+                }
+                
+            }
+        }//if not grabbed right now, but grabbed is true, reset it.
+        else if (!grabScript.isGrabbed && grabbed)
+        {
+            grabbed = false;
+            rb.WakeUp();
+            agent.enabled = true;
+        }
+    }
+
+    void OnCollisionEnter (Collision coll)
+    {
+        Debug.Log(coll.gameObject.name);
+        GameObject tempObject = coll.gameObject;
+        if (tempObject.name == "knife")
+        {
+            knifeGrab = true;
+            Rigidbody tempRigid = coll.gameObject.GetComponent<Rigidbody>();
+
+            //disable collider
+            tempObject.GetComponent<BoxCollider>().enabled = false;
+
+            //freeze position of knife.
+            tempRigid.constraints = RigidbodyConstraints.FreezePosition; //to unfreeze, tempRigid.constraints = RigidbodyConstraints.None
+            GameManager.Instance.hasKnife = true;
+        }
+
+        //if has knife and reaches outlet, game over.
+        if (GameManager.Instance.hasKnife && coll.gameObject.name == "Outlet")
+        {
+            GameManager.Instance.GameOver();
+        }
+    }
+
+    IEnumerator KnifeDrop()
+    {
+        knifeTrans.gameObject.GetComponent<Rigidbody>().Sleep();
+
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("Got to Knife Drop");
+
+        knifeTrans.gameObject.GetComponent<Rigidbody>().WakeUp();
+        knifeTrans.gameObject.GetComponent<BoxCollider>().enabled = true;
+        knifeTrans.position +=  new Vector3(.2f, 0);
+        //if below the floor, bring back up.
+        if (knifeTrans.position.y < 0)
+        {
+            knifeTrans.position -= new Vector3(0, knifeTrans.position.y + .02f);
+        }
+    }
+}
---
+++ GameManager.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+    public GameObject player;
+    public Transform knifePos;
+    public Transform outletPos;
+    public Transform playerPos;
+    public GameObject baby;
+    public bool hasKnife = false;
+    public Timer timerScript;
+    public Text menuText;
+
+    private Baby babyScript;
+    private Save save;
+
+    void Start()
+    {
+        //instantiate singleton
+        if (Instance == null)
+        {
+            Instance = this;
+        } else if (Instance != this) {
+            Destroy(gameObject);
+        }
+
+        babyScript = baby.GetComponent<Baby>();
+        save = LoadSave();
+
+        //plays intro
+        StartCoroutine(OpeningWindow());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if start button is pressed, exit game
+        if (OVRInput.GetUp(OVRInput.Button.Start))
+        {
+            Debug.Log("Start Button Pressed");
+            Application.Quit();
+        }
+       
+        //if baby doesn't have the knife, it goes for the knife
+        if (!hasKnife)
+        {
+            babyScript.target = knifePos;
+        } else
+        {
+            babyScript.target = outletPos;
+        }
+    }
+
+    public void GameOver ()
+    {
+        if (timerScript.gameOver == false) //if you lost
+        {
+            //set the text to you lose.
+            timerScript.GameLost();
+            save.losses += 1;
+        } else
+        {
+            save.wins += 1;
+        }
+        //Save the game
+        SaveGame();
+
+        //start a coroutine that waits for 5 seconds before closing the game
+        StartCoroutine(Exit());
+    }
+    
+    IEnumerator Exit ()
+    {
+        yield return new WaitForSecondsRealtime(5);
+        Debug.Log("Just about to quit");
+        Application.Quit();
+    }
+
+    private void SaveGame()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, save);
+        file.Close();
+    }
+
+    private Save LoadSave()
+    {
+        if (File.Exists(Application.persistentDataPath + "/gamesave.save"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
+            save = (Save)bf.Deserialize(file);
+            file.Close();
+        } else
+        {
+            Debug.Log("No data loaded");
+            save = new Save();
+        }
+
+        return save;
+    }
+
+    //the ui window with the Game Title, win loss Record, and how to exit the game
+    IEnumerator OpeningWindow()
+    {
+        //activate the canvas the text is a child of.
+        timerScript.enabled = false;
+        babyScript.enabled = false;
+
+        menuText.text = "Welcome to Parenting Simulator!";
+        yield return new WaitForSecondsRealtime(5);
+        menuText.text = "Keep your baby from hurting itself until the timer runs out.";
+        yield return new WaitForSecondsRealtime(5);
+        menuText.text = "To exit the game, press the start button at any time.";
+        yield return new WaitForSecondsRealtime(5);
+        menuText.text = "Your current stats are " + save.wins + " win(s) and " + save.losses + " loss(es).";
+        yield return new WaitForSecondsRealtime(5);
+        menuText.text = "Ready?";
+        yield return new WaitForSecondsRealtime(3);
+        menuText.text = "3";
+        yield return new WaitForSecondsRealtime(1);
+        menuText.text = "2";
+        yield return new WaitForSecondsRealtime(1);
+        menuText.text = "1";
+        yield return new WaitForSecondsRealtime(1);
+        menuText.text = "GO!";
+        yield return new WaitForSecondsRealtime(1);
+
+        timerScript.enabled = true;
+        babyScript.enabled = true;
+    }
+}
